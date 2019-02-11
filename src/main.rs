bc5fd4ff3ee0c4059ef3e3f66582b3f3dfb7acd1
Romain Vimont <rom@rom1v.com>
2019-02-11 14:48:11 +0100

    Initial example

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use std::marker::PhantomData;
+
+struct SliceField<'a> {
+    ptr: *mut i32,
+    len: usize,
+    field: usize,
+    marker: PhantomData<&'a mut i32>,
+}
+
+impl SliceField<'_> {
+    fn inc(&mut self) {
+        unsafe {
+            for i in (self.field..self.len).step_by(2) {
+                *self.ptr.add(i) += 1;
+            }
+        }
+    }
+
+    fn dec(&mut self) {
+        unsafe {
+            for i in (self.field..self.len).step_by(2) {
+                *self.ptr.add(i) -= 1;
+            }
+        }
+    }
+}
+
+unsafe impl Send for SliceField<'_> {}
+
+fn split_fields(array: &mut [i32]) -> (SliceField<'_>, SliceField<'_>) {
+    (
+        SliceField {
+            ptr: array.as_mut_ptr(),
+            len: array.len(),
+            field: 0,
+            marker: PhantomData,
+        },
+        SliceField {
+            ptr: array.as_mut_ptr(),
+            len: array.len(),
+            field: 1,
+            marker: PhantomData,
+        },
+    )
+}
+
+fn main() {
+    let data = &mut [0i32; 11];
+    {
+        let (mut even, mut odd) = split_fields(data);
+        rayon::join(|| even.inc(), || odd.dec());
+    }
+    println!("{:?}", data);
+}
